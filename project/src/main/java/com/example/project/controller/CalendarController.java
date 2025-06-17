9e7210e80fafeb19d073472f703f696403b36834
YahuiCui <nkcyh510@gmail.com>
2025-06-17 14:12:08 +0900

    html-sai

--- project/src/main/java/com/example/project/controller/CalendarController.java
+++ project/src/main/java/com/example/project/controller/CalendarController.java
@@ -1,19 +1,11 @@
+
 package com.example.project.controller;
 
 import java.time.YearMonth;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class CalendarController {
 
-    @Autowired
-    private CalendarService calendarService;
-
-    @GetMapping("/calendar")
-    public String showCalendar(){
-
-        if(month == null) month = YearMonth.now();
-
-    }
+    
 }
