52bc760aa4f2a0568f473e7a2589c070bcd3000e
Tea202504 <aston.selly.2022@gmail.com>
2025-06-20 11:33:22 +0900

    home1

--- project/src/main/java/com/example/project/controller/HomeController.java
+++ project/src/main/java/com/example/project/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.example.project.controller;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String homePage() {
-        return "home";
+    public String homePage(Model model) {
+        DayOfWeek today = LocalDate.now().getDayOfWeek(); // 今日の日付の曜日を取得
+        model.addAttribute("dayOfWeek", today); // モデルに曜日を追加
+        return "home"; // home.html テンプレートを返す
     }
 }
